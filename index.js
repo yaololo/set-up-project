6ca74e68c75ca9645a576f2d828ffbbdf3ab53e5
zhuangyongming <yongming.zhuang@moneysmart.com>
2019-05-04 00:11:58 +0800

    Finish setup

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const cowsay = require('cowsay')
 const cors = require('cors')
+const path = require('path')
+
 // Create the server
 const app = express()
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
   try {
