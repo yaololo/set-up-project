38a794a7ea54f170f60c7953d4d19e65c318c311
zhuangyongming <yongming.zhuang@moneysmart.com>
2019-05-04 12:07:36 +0800

    Fix routing issue

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const app = express()
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
-// Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
-
 
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
@@ -33,6 +28,12 @@ app.get('/api/cow/', cors(), async (req, res, next) => {
     next(err)
   }
 })
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
