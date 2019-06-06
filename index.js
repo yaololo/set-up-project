cbe1209afeb01eac2ad8cd97ec837824a49e24e0
zhuangyongming <yongming.zhuang@moneysmart.com>
2019-06-07 00:12:43 +0800

    Clean up index file

--- index.js
+++ index.js
@@ -2,33 +2,14 @@ const express = require('express');
 const cowsay = require('cowsay');
 const cors = require('cors');
 const path = require('path');
+const routes = require('./server/api');
 
 // Create the server
 const app = express();
 
+app.use(routes);
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-// Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
-  try {
-    const text = req.params.say;
-    const moo = cowsay.say({ text });
-    res.json({ moo });
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Serve our base route that returns a Hello World cow
-app.get('/api/cow/', cors(), async (req, res, next) => {
-  try {
-    const moo = cowsay.say({ text: 'Hello World!' })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
