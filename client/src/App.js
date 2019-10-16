f1b86d68b51741cc0d57c2b6e2dcde346288288e
zhuang yongming <zhuangyongming1900@gmail.com>
2019-10-17 02:40:45 +0800

    Add ping my self feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
-import Login from "./components/login";
 import Carousels from "./components/carousels";
 
 class App extends Component {
+
   render() {
     return (
       <>
--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-const express = require('express')
-const cowsay = require('cowsay')
-const cors = require('cors')
-const path = require('path')
+const express = require("express");
+const cowsay = require("cowsay");
+const cors = require("cors");
+const path = require("path");
+var http = require("http");
 
 // Create the server
-const app = express()
+const app = express();
+const drinkCoffee = setInterval(function() {
+  console.log('Started');
+  http.get("https://mock-api-1900.herokuapp.com");
+}, 600000);
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
+app.get("/api/cow/:say", cors(), async (req, res, next) => {
   try {
-    const text = req.params.say
-    const moo = cowsay.say({ text })
-    res.json({ moo })
+    const text = req.params.say;
+    const moo = cowsay.say({ text });
+    res.json({ moo });
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 // Serve our base route that returns a Hello World cow
-app.get('/api/cow/', cors(), async (req, res, next) => {
+app.get("/api/cow/", cors(), async (req, res, next) => {
   try {
-    const moo = cowsay.say({ text: 'Hello World!' })
-    res.json({ moo })
+    const moo = cowsay.say({ text: "Hello World!" });
+    res.json({ moo });
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 // Choose the port and start the server
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Mixing it up on port ${PORT}`)
-})
+  console.log(`Mixing it up on port ${PORT}`);
+  drinkCoffee();
+});
