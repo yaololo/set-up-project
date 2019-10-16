1747e7cda54493194a66079886001d2bf84192ef
zhuang yongming <zhuangyongming1900@gmail.com>
2019-10-17 02:00:53 +0800

    Create login and carousel components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Login from "./components/login";
+import Carousels from "./components/carousels";
 
 class App extends Component {
   render() {
-    return <Login></Login>;
+    return (
+      <>
+        <Carousels></Carousels>
+      </>
+    );
   }
 }
 export default App;
