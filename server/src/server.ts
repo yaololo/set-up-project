4e9ddbdc1393c3c6700f28d27ae88f8c7fe241fa
zhuang yongming <zhuangyongming1900@gmail.com>
2019-12-29 13:19:53 +0800

    Create server entry file server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,33 @@
-console.log('started');
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import path from "path";
+import { db } from "./config";
+
+const app = express();
+
+// parse application/x-www-form-urlencoded and parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+// Choose the port and start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Mixing it up on port ${PORT}`);
+
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  db.once("open", () => {
+    console.log("DB connected successfully");
+  });
+});
