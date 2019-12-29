076c6fcce2b0dae4d55ca8f22689e5f66e975cd5
zhuang yongming <zhuangyongming1900@gmail.com>
2019-12-29 16:54:19 +0800

    Add config utils file

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,7 +2,6 @@ import { envConfig } from "./env-config";
 import db from "./db-setup";
 
 if (envConfig.isLocal) {
-  require("dotenv").config();
 } else if (envConfig.isTest) {
   /*
    ** TODO: add test config
---
+++ server/src/config/utils.ts
@@ -0,0 +1,9 @@
+const checkRequiredEnvironment = (variables: Array<string>) => {
+  variables.forEach(name => {
+    if (!process.env[name]) {
+      throw new Error(`Environment variable ${name} is missing`);
+    }
+  });
+};
+
+export { checkRequiredEnvironment };
