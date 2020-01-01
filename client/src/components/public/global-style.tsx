156c5e45fd7e5f1177a4e00c85b13bf352fa3363
zhuang yongming <zhuangyongming1900@gmail.com>
2020-01-01 11:45:00 +0800

    Remove auto fill style

--- client/src/components/public/global-style.tsx
+++ client/src/components/public/global-style.tsx
@@ -8,6 +8,12 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     height: 100%;
     overflow: auto;
+    input:-webkit-autofill,
+    input:-webkit-autofill:hover, 
+    input:-webkit-autofill:focus, 
+    input:-webkit-autofill:active  {
+        -webkit-box-shadow: 0 0 0 30px white inset !important;
+    }
   }
 `;
 
