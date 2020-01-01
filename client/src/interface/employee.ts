1125ed298b3810ebc0c1ed11e6c1b3f67e5e117b
zhuang yongming <zhuangyongming1900@gmail.com>
2020-01-01 11:46:11 +0800

    Remove employee interface

--- client/src/interface/employee.ts
+++
@@ -1,4 +0,0 @@
-type IDirectSubordinates = { "direct-subordinates": string[] };
-type IEmployeeResponse = [string, IDirectSubordinates | undefined];
-
-export { IDirectSubordinates, IEmployeeResponse };
