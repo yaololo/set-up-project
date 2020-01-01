2573837da6ccd39c819160b103d3a6a155b58c18
zhuang yongming <zhuangyongming1900@gmail.com>
2020-01-01 11:46:36 +0800

    Render login component

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import GlobalStyle from "./components/public/global-style";
-
+import Login from "./components/login";
 class app extends Component {
   render() {
     return (
       <>
         <GlobalStyle />
+        <Login />
       </>
     );
   }
