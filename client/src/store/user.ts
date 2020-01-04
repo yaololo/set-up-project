e7df76f1b6ceefe469648486f47951e68e1f7350
zhuang yongming <zhuangyongming1900@gmail.com>
2020-01-04 22:26:41 +0800

    Create user store

---
+++ client/src/store/user.ts
@@ -0,0 +1,13 @@
+import { observable } from "mobx";
+
+class UserStore {
+  // @observable userProfile = {
+  //   email: '',
+  // }
+
+  login = () => {};
+}
+
+const userStore = new UserStore();
+
+export default userStore;
