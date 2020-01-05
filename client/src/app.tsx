ee5221ca9533cfdc4031c95f921fbf66c79ff581
zhuang yongming <zhuangyongming1900@gmail.com>
2020-01-05 22:35:58 +0800

    Create error boundary component

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import GlobalStyle from "./components/public/global-style";
 import Login from "./components/login";
+import ErrorBoundary from "components/error-boundary";
 class app extends Component {
   render() {
     return (
       <>
-        <GlobalStyle />
-        <Login />
+        <ErrorBoundary>
+          <GlobalStyle />
+          <Login />
+        </ErrorBoundary>
       </>
     );
   }
