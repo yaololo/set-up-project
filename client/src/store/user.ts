00fc2a19c53d496da6585963fc8454e5324589cd
zhuang yongming <zhuangyongming1900@gmail.com>
2020-01-05 15:39:08 +0800

    Create user store with login function

--- client/src/store/user.ts
+++ client/src/store/user.ts
@@ -1,11 +1,21 @@
 import { observable } from "mobx";
-
+import { IFormValues } from "interface/login";
+import { ajax } from "lib/axios";
+import { ICustomizedError } from "interface/axios";
 class UserStore {
   // @observable userProfile = {
   //   email: '',
   // }
 
-  login = () => {};
+  login = async (payload: IFormValues) => {
+    try {
+      await ajax.post("/user/login", payload);
+      return true;
+    } catch (e) {
+      console.log(e);
+      // return e as ICustomizedError;
+    }
+  };
 }
 
 const userStore = new UserStore();
