50816bf37940239766043265f55238b82b67e7b3
zhuang yongming <zhuangyongming1900@gmail.com>
2020-01-05 15:37:57 +0800

    Fix proxy config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -16,7 +16,11 @@ module.exports = {
 
   // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
   },
   // context: path.resolve(__dirname, "src"),
   module: {
@@ -36,6 +40,42 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       }
     ]
+  },
+  devServer: {
+    // contentBase: path.resolve(__dirname, "dist"),
+    // publicPath: "/",
+    // headers: {
+    //   "Service-Worker-Allowed": "/"
+    // },
+    // historyApiFallback: {
+    //   rewrites: [
+    //     {
+    //       from: /^\/$/,
+    //       to: "/index.html"
+    //     },
+    //     {
+    //       from: /./,
+    //       to: "/index.html"
+    //     }
+    //   ]
+    // },
+    // inline: true,
+    // watchOptions: {
+    //   watch: true
+    // },
+    // disableHostCheck: true,
+    // compress: true,
+    hot: true,
+    // contentBase: resolve(__dirname, "dist"),
+    publicPath: "/",
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+        xfwd: false
+      }
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
