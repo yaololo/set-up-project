b95a6a1398a7f7215344acd07c3b4984295e7b65
zhuang yongming <zhuangyongming1900@gmail.com>
2020-01-06 00:44:38 +0800

    Serve routes component

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import GlobalStyle from "./components/public/global-style";
 import Login from "./components/login";
 import ErrorBoundary from "components/error-boundary";
+import Routes from "components/routes";
+import { BrowserRouter } from "react-router-dom";
+
 class app extends Component {
   render() {
     return (
       <>
         <ErrorBoundary>
           <GlobalStyle />
-          <Login />
+          <BrowserRouter>
+            <Routes></Routes>
+          </BrowserRouter>
         </ErrorBoundary>
       </>
     );
