b7a5f20dc369a6d1b50aae1de8407c6546cc7603
zhuang yongming <zhuangyongming1900@gmail.com>
2020-01-06 01:09:15 +0800

    Temporary comment out bundle output in webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,16 +9,17 @@ module.exports = {
   // },
 
   // and output it into /dist as bundle.js
-  output: {
-    // `filename` provides a template for naming your bundles (remember to use `[name]`)
-    filename: "[name].bundle.js",
-    // `chunkFilename` provides a template for naming code-split bundles (optional)
-    chunkFilename: "[name].bundle.js",
-    // `path` is the folder where Webpack will place your bundles
-    path: path.join(__dirname, "/dist"),
-    // `publicPath` is where Webpack will load your bundles from (optional)
-    publicPath: "dist/"
-  },
+
+  // output: {
+  //   // `filename` provides a template for naming your bundles (remember to use `[name]`)
+  //   filename: "[name].bundle.js",
+  //   // `chunkFilename` provides a template for naming code-split bundles (optional)
+  //   chunkFilename: "[name].bundle.js",
+  //   // `path` is the folder where Webpack will place your bundles
+  //   path: path.join(__dirname, "/dist"),
+  //   // `publicPath` is where Webpack will load your bundles from (optional)
+  //   publicPath: "dist/"
+  // },
 
   // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
   resolve: {
