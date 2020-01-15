6a7def7d2f3a35ff440e6e2e22eb7c6d9a4b4ee9
zhuang yongming <zhuangyongming1900@gmail.com>
2020-01-15 18:16:13 +0800

    Add header bar styles

--- client/src/components/main/dashboard-header.tsx
+++ client/src/components/main/dashboard-header.tsx
@@ -9,27 +9,65 @@ import {
 import MenuIcon from "@material-ui/icons/Menu";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import clsx from "clsx";
-import { useStyles } from "./style";
+import { makeStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 240;
 
 interface IProps {
   isDrawerOpen: boolean;
   handleDrawerOpen: (flag: boolean) => void;
 }
 
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButtonHidden: {
+    display: "none"
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  menuButton: {
+    marginRight: 36
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
 const DashboardHeader: React.FC<IProps> = props => {
   const classes = useStyles();
   return (
     <AppBar
       position="absolute"
-      className={clsx(classes.appBar, open && classes.appBarShift)}
+      className={clsx(
+        classes.appBar,
+        props.isDrawerOpen && classes.appBarShift
+      )}
     >
       <Toolbar className={classes.toolbar}>
         <IconButton
           edge="start"
           color="inherit"
           aria-label="open drawer"
-          onClick={() => props.handleDrawerOpen(!props.isDrawerOpen)}
-          className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+          onClick={() => props.handleDrawerOpen(true)}
+          className={clsx(
+            classes.menuButton,
+            props.isDrawerOpen && classes.menuButtonHidden
+          )}
         >
           <MenuIcon />
         </IconButton>
