800ea35893586e060aeb625f210db683f73a697a
yongming <zhuangyongming1900@gmail.com>
2020-02-25 18:46:06 +0800

    Add fetch user profile method in user store

--- client/src/store/user.ts
+++ client/src/store/user.ts
@@ -1,11 +1,23 @@
-import { observable } from "mobx";
+import { observable, action } from "mobx";
 import { IFormValues } from "interface/login";
 import { ajax } from "lib/axios";
 import { ICustomizedError } from "interface/axios";
+import { IUserProfile, IProfileResponse } from "interface/user";
+
 class UserStore {
-  // @observable userProfile = {
-  //   email: '',
-  // }
+  @observable userProfile: IUserProfile | null = null;
+
+  getProfile = async () => {
+    try {
+      const {
+        data: { data }
+      } = await ajax.get<IProfileResponse>("/user/profile");
+      this.setUserProfile(data);
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  };
 
   login = async (payload: IFormValues) => {
     try {
@@ -16,6 +28,10 @@ class UserStore {
       return false;
       // return e as ICustomizedError;
     }
+  };
+
+  @action setUserProfile = (profile: IUserProfile) => {
+    this.userProfile = profile;
   };
 }
 
