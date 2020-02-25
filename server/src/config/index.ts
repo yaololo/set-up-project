d8428f9630008ccfdc390f6a9aaad2702a408c2e
yongming <zhuangyongming1900@gmail.com>
2020-02-25 18:02:03 +0800

    Skip port env checking and connect to db after server started

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,7 +2,8 @@ import { envConfig } from "./env-config";
 import db from "./db-setup";
 import { checkRequiredEnvironment } from "./utils";
 
-const env = ["PORT", "DB_PATH", "NODE_ENV", "PRIVATE_KEY"];
+// const env = ["PORT", "DB_PATH", "NODE_ENV", "PRIVATE_KEY"];
+const env = ["DB_PATH", "NODE_ENV", "PRIVATE_KEY"];
 checkRequiredEnvironment(env);
 
 export { envConfig, db, checkRequiredEnvironment };
