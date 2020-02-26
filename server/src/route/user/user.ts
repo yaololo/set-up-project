b58f1b4fe09b95e1a02f8f7f03dd4d28cf70abb4
yongming <zhuangyongming1900@gmail.com>
2020-02-26 22:53:14 +0800

    Add fallback route and get user profile route in user routes

--- server/src/route/user/user.ts
+++ server/src/route/user/user.ts
@@ -1,8 +1,14 @@
 import { route } from "../../lib/router";
 import signupController from "./signup-controller";
 import loginController from "./login-controller";
+import { verifyJWT } from "../../middleware/jwt-middleware";
+import getProfileController from "./get-profile-controller";
 
 route.post("/user/signup", signupController);
 route.post("/user/login", loginController);
+route.get("/user/profile", verifyJWT, getProfileController);
+route.use("/user/*", (req, res) =>
+  res.status(404).json({ message: "Invalid operation" })
+);
 
 export default route;
