b819d4eccd016e558a6b8bcfd527382e09fbca42
yongming <zhuangyongming1900@gmail.com>
2020-02-26 22:53:54 +0800

    Modify error message data type in jwt middleware

--- server/src/middleware/jwt-middleware.ts
+++ server/src/middleware/jwt-middleware.ts
@@ -4,10 +4,11 @@ import jwt from "jsonwebtoken";
 const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
   try {
     let decoded = jwt.verify(req.cookies.token, process.env.PRIVATE_KEY!);
+    console.log(decoded);
     res.locals.userInfo = decoded;
     next();
   } catch (e) {
-    return res.status(401).json({ e });
+    return res.status(401).json({ message: JSON.stringify(e) });
   }
 };
 export { verifyJWT };
