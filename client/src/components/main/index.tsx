67e8acc0fac03826acfc57a1c6ee555bde755194
yongming <zhuangyongming1900@gmail.com>
2020-03-08 16:38:03 +0800

    Check if user profile is set in main component

--- client/src/components/main/index.tsx
+++ client/src/components/main/index.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import DashboardHeader from "./dashboard-header";
 import SideMenu from "./side-menu";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import MainContent from "./main";
+import userStore from "@/store/user";
+import { useObserver } from "mobx-react";
+import { useHistory } from "react-router-dom";
 
 const Main = () => {
   const [open, setOpen] = React.useState(true);
@@ -14,8 +17,23 @@ const Main = () => {
   }));
 
   const classes = useStyles();
+  const history = useHistory();
 
-  return (
+  const userAuthentication = useCallback(async () => {
+    if (!userStore.isProfileSet()) {
+      try {
+        await userStore.getProfile();
+      } catch (e) {
+        history.push("/login");
+      }
+    }
+  }, [userStore.userProfile]);
+
+  useEffect(() => {
+    userAuthentication();
+  }, []);
+
+  return useObserver(() => (
     <div className={classes.root}>
       <CssBaseline />
       <DashboardHeader
@@ -25,7 +43,7 @@ const Main = () => {
       <SideMenu isOpen={open} handleClose={setOpen} />
       <MainContent />
     </div>
-  );
+  ));
 };
 
 export default Main;
