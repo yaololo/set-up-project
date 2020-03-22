3c821f1f0b025bba832ee2010f9f0195b7cd7c49
yongming <zhuangyongming1900@gmail.com>
2020-03-22 14:23:11 +0800

    Put more details in console log

--- server/src/config/utils.ts
+++ server/src/config/utils.ts
@@ -1,6 +1,8 @@
 const checkRequiredEnvironment = (variables: Array<string>) => {
   console.log(variables);
+  console.log(variables.length);
   variables.forEach(name => {
+    console.log(name);
     if (!process.env[name]) {
       throw new Error(`Environment variable ${name} is missing`);
     }
