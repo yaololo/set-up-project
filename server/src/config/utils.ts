b2fa3f6f16d495b3e05cf28505048ac0bf94c5ae
yongming <zhuangyongming1900@gmail.com>
2020-03-22 14:37:12 +0800

    Fix server error

--- server/src/config/utils.ts
+++ server/src/config/utils.ts
@@ -3,7 +3,9 @@ const checkRequiredEnvironment = (variables: Array<string>) => {
   console.log(variables.length);
   variables.forEach(name => {
     console.log(name);
-    if (!process.env[name]) {
+    console.log(process.env[`${name}`]);
+    console.log(process.env[name]);
+    if (!process.env[`${name}`]) {
       throw new Error(`Environment variable ${name} is missing`);
     }
   });
